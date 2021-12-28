3a574211d8ecdb47901ce651bbdacf712e5b552a
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-12-28 16:56:10 +0530

    Update Cadd.py

--- Cadd.py
+++ Cadd.py
@@ -1,15 +1,13 @@
 from telethon.sync import TelegramClient
 from telethon import functions, type
+from telethon.function.messages import GetCommonChatsRequest
 
 op = GetCommonChatsRequest
 
-if user_id in op
+if user_id in op:
     try:
-    	     await borg(
-                        functions.messages.AddChatUserRequest in op(
-                            chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000
-                        )
-                    )
-                except Exception as e:
-                    await event.reply(str(e))
-            await edit_or_reply(event, "Invited Successfully")
+        await borg(functions.messages.AddChatUserRequest in op(chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000))
+        return 
+    except Exception as e:
+        await event.reply(str(e))
+        await edit_or_reply(event, "Invited Successfully")
